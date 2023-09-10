5deecc86e84fb1fa5f4393543ee54035c8e9289b
Git-Balance <davidbond001@icloud.com>
2023-09-09 20:25:11 -0700

    Finished the script that will be ran at startup
    The script is basically finished. It displays 2 widgets, calendarWidget and statusWidget
    calendarWidget displays a calendar
    statusWidget displays the current date, time, and battery percentage
    
    Additionally, added a Future Plans section to Plan.md

--- startup-widget/Plan.md
+++ startup-widget/Plan.md
@@ -1,12 +1,19 @@
 # Plan for startup-widget
 
 ## General notes
-- Make a simple "widget" (like the one in `dunesh`) that runs on the creation of a terminal
+- Make a ~~simple "widget"~~ have multiple widgets (like the one in `dunesh`) that run on the creation of a terminal
 - It should look similar to `dunesh` or `neofetch`
 ## What it will have
 - A calendar
 - Current date and time
 - Battery percentage
 - Uptime
-- Wifi and bluetooth status
+- ~~Wifi and bluetooth status~~
+## Future Plans
+- [ ] Make a function that places multiline commands like `cal` side by side, so that the widgets are able to be placed better
+EX:
+|---------| |------------|
+ 24 of May   100% Battery
+|---------| |------------|
 
+- [ ] After making the previous plan's function, separate statusWidget into 2 separate widgets
--- startup-widget/renamethisscript.sh
+++ startup-widget/renamethisscript.sh
@@ -1,3 +1,59 @@
 #!/usr/bin/env bash
 
+boxer()
+{
+    local boxType=$1
+    local boxLength=$2
+    local boxPrint=''
+    local boxStart=''
+    local boxEnd=''
 
+    if [ "$boxType" == "up" ]
+    then
+        boxStart='└'
+        boxEnd='┘'
+    elif [ "$boxType" == "down" ]
+    then
+        boxStart='┌'
+        boxEnd='┐'
+    else
+        echo 'Invalid box type'
+    fi
+
+    boxPrint=$boxStart
+    for (( i = 0; i < $boxLength - 2; i++ ))
+    do
+        boxPrint=$boxPrint'─'
+    done
+    boxPrint=$boxPrint$boxEnd
+
+    echo $boxPrint
+}
+
+calendarWidget()
+{
+    local boxSize=22
+    
+    boxer down $boxSize
+    cal | sed 's/^/ /' | cat
+    boxer up $boxSize
+    
+    # Just some tests that are here for reference 
+    #
+    # boxer down 22
+    # cal 2 Jan 2022 | sed 's/^/ /' | cat
+    # boxer up 20
+    # echo "\ttest" | cat
+}
+
+statusWidget()
+{
+    local boxSize=42
+
+    boxer down $boxSize
+    echo "$(date "+ %a, %b of %d | %H:%M") | Battery at $(cat /sys/class/power_supply/BAT0/capacity)%"
+    boxer up $boxSize
+}
+
+statusWidget
+calendarWidget
